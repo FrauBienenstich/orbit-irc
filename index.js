305f02bac71989e11c483ae94e77838ab6849ba7
FrauBienenstich <susanne.dewein@gmail.com>
2016-08-06 17:15:09 +0200

    pass channel name as command line arg

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var irc = require('irc');
+var channel = process.argv[2] || 'testchannel'
 var ircClient = new irc.Client('irc.freenode.net', 'MrsOrbit', {
-    channels: ['#testchannel'],
+    channels: ["#" + channel],
 });
 
 var Orbit = require('./Orbit');
@@ -8,17 +9,16 @@ var Ipfs = require('ipfs-api');
 
 var orbitClient = new Orbit(new Ipfs());
 
-
 // talking to Orbit
 orbitClient.connect('178.62.241.75:3333', 'MrsOrbit', '').then(function (result) {
   console.log("CONNECTED");
-  orbitClient.join('testchannel');
+  orbitClient.join(channel);
   orbitClient.events.on('synced', function (channel, messages) {
     console.log("MESSAGES:", JSON.stringify(messages, null, 2));
     messages.forEach((message) => { 
       orbitClient.getPost(message.payload.value).then(function (post) {
         console.log(`content: ${post.content}` );
-        ircClient.say('#testchannel', `Orbit <${post.meta.from}> ${post.content}`);
+        ircClient.say('#' + channel, `Orbit <${post.meta.from}> ${post.content}`);
       });
       // returns something like {"Links":[],"Data":"{\"content\":\"hallo\",\"meta\":{\"type\":\"text\",\"size\":15,\"ts\":1470483208976,\"from\":\"name\"}}"}
     });
@@ -29,7 +29,7 @@ orbitClient.connect('178.62.241.75:3333', 'MrsOrbit', '').then(function (result)
 ircClient.addListener('message', function (from, to, message) {
     var userName = from;
     console.log(from + ' => ' + to + ': ' + message);
-    orbitClient.send('testchannel', `IRC <${from}> ${message}`);
+    orbitClient.send(channel, `IRC <${from}> ${message}`);
 });
 
 ircClient.addListener('error', function(message) {
