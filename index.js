77f42f7e1ad0cdb1b4969caf3c9561924423b825
haad <haad@headbanggames.com>
2016-08-08 17:12:06 +0200

    Fix format

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-var irc = require('irc');
-var channel = process.argv[2] || 'testchannel'
-var ircClient = new irc.Client('irc.freenode.net', 'MrsOrbit', {
-    channels: ["#" + channel],
-});
-
+const irc = require('irc');
 const OrbitDB = require('orbit-db');
 const Post = require('ipfs-post');
-var Ipfs = require('ipfs-api');
+const Ipfs = require('ipfs-api');
+
+var channel = process.argv[2] || 'testchannel'
+
+var ircClient = new irc.Client('irc.freenode.net', 'MrsOrbit', {
+  channels: ["#" + channel],
+});
 
 var ipfs = new Ipfs();
 var orbitdb;
@@ -20,11 +21,12 @@ var sanitizeChannelName = (ircChannel) => {
 
 // writing message from  irc into orbit db
 var createMessage = (ipfs, message, from, to) => {
-  const data = {
-      content: message,
-      from: from
-    };
   var sanitizedChannel = sanitizeChannelName(to)
+
+  const data = {
+    content: message,
+    from: from
+  };
 
   // adds a message to the db
   Post.create(ipfs, Post.Types.Message, data)
@@ -36,8 +38,8 @@ OrbitDB.connect('178.62.241.75:3333', 'MrsOrbit', '', ipfs).then((res) => orbitd
 
 ircClient.addListener('join', function (channelName, nick, message) {
   // notifications in orbit and irc that connection has happened:
-  var ircText = `has now connected this channel with Orbit.`
   var orbitText = `/me has now connected this channel with IRC.`
+  var ircText = `has now connected this channel with Orbit.`
   createMessage(ipfs, orbitText, nick, channelName)
   ircClient.action(channelName, ircText)
 
@@ -50,12 +52,11 @@ ircClient.addListener('join', function (channelName, nick, message) {
 
 //  by writing directly to the db user can be other than MrsOrbit (which is set initially when connecting to OrbitDB), 'from' gets set dynamically now depending on irc username who sends message
 ircClient.addListener('message', function (from, to, message) {
-    console.log(from + ' => ' + to + ': ' + message);
-
-    createMessage(ipfs, message, from, to);
+  console.log(from + ' => ' + to + ': ' + message);
+  createMessage(ipfs, message, from, to);
 });
 
 ircClient.addListener('error', function(message) {
-    console.log('error: ', message);
+  console.log('error: ', message);
 });
 
